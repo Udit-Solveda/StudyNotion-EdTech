a9d6c2db766c7ff63c158e3180f27cddf188dbd6
Udit-Solveda <umaheshwari@solveda.com>
2023-12-21 16:04:43 +0530

    Publish -course,course builder form

--- src/components/core/Dashboard/AddCourse/CourseBuilderForm/NestedView.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilderForm/NestedView.jsx
@@ -1,28 +1,58 @@
 import { useState } from "react"
-import { AiFillCaretDown, AiOutlinePlus } from "react-icons/ai"
+import { AiFillCaretDown } from "react-icons/ai"
 import { FaPlus } from "react-icons/fa"
 import { MdEdit } from "react-icons/md"
 import { RiDeleteBin6Line } from "react-icons/ri"
 import { RxDropdownMenu } from "react-icons/rx"
 import { useDispatch, useSelector } from "react-redux"
+
+import {
+  deleteSection,
+  deleteSubSection,
+} from "../../../../../services/operations/courseDetailsAPI"
+import { setCourse } from "../../../../../slices/courseSlice"
+import ConfirmationModal from "../../../../common/ConfirmationModal"
 import SubSectionModal from "./SubSectionModal"
 function NestedView({handleChangeEditSectionName}) {
-  const { course } = useSelector((state) => state.course);
-  const { token } = useSelector((state) => state.auth);
-  const [loading, setLoading] = useState(false);
-  const [editSectionName, setEditSectionName] = useState(null);
-  const [addSubSection, setAddSubsection] = useState(null);
-  const [viewSubSection, setViewSubSection] = useState(null);
-  const [editSubSection, setEditSubSection] = useState(null);
+  const { course } = useSelector((state) => state.course)
+  const { token } = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
+  // States to keep track of mode of modal [add, view, edit]
+  const [addSubSection, setAddSubsection] = useState(null)
+  const [viewSubSection, setViewSubSection] = useState(null)
+  const [editSubSection, setEditSubSection] = useState(null)
   // to keep track of confirmation modal
-  const [confirmationModal, setConfirmationModal] = useState(null);
+  const [confirmationModal, setConfirmationModal] = useState(null)
+  
 
-  const handleDeleleSection = ()=>{
-
+  const handleDeleleSection = async (sectionId) => {
+    const result = await deleteSection({
+      sectionId,
+      courseId: course._id,
+      token,
+    })
+    if (result) {
+      dispatch(setCourse(result))
+    }
+    setConfirmationModal(null)
   }
-  const handleDeleteSubSection = ()=>{}
+
+  const handleDeleteSubSection = async (subSectionId, sectionId) => {
+    const result = await deleteSubSection({ subSectionId, sectionId, token })
+    if (result) {
+      // update the structure of course
+      const updatedCourseContent = course.courseContent.map((section) =>
+        section._id === sectionId ? result : section
+      )
+      const updatedCourse = { ...course, courseContent: updatedCourseContent }
+      dispatch(setCourse(updatedCourse))
+    }
+    setConfirmationModal(null)
+  }
+
   return (
     <div>
+    
       <div>
         {course.courseContent?.map((section) => (
           <details key={section._id} open>
@@ -130,11 +160,11 @@ function NestedView({handleChangeEditSectionName}) {
         <></>
       )}
       {/* Confirmation Modal */}
-      {/* {confirmationModal ? (
+      {confirmationModal ? (
         <ConfirmationModal modalData={confirmationModal} />
       ) : (
         <></>
-      )} */}
+      )}
     </div>
   );
 }
